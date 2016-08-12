6e11feab0344159ca121b5a2041dd0022c4454ff
Sean Niesen <seanniesen@gmail.com>
2016-08-12 16:13:31 -0700

    fixed sunrise

--- sunrise/app.js
+++ sunrise/app.js
@@ -2,7 +2,7 @@ var app = angular.module("sunrise-times", ['ui.bootstrap', 'sunrise-times.servic
 
 app.controller('MainCtrl', [
 	'$scope', 'dthreeService',
-	function($scope, dthreeService){
+	function($scope, dthreeService) {
 
   	$scope.open = function($event, opened) {
 	    $event.preventDefault();
@@ -35,90 +35,127 @@ app.controller('MainCtrl', [
 
 			var geocoder = new google.maps.Geocoder();
 			geocoder.geocode( { 'address': address}, function(results, status) {
-  			if (status == google.maps.GeocoderStatus.OK)
-  			{
-   			  var i = 0;
-   			  var timesArray = [];
+  				if (status == google.maps.GeocoderStatus.OK)
+  				{
+   			  		var i = 0;
+   			  		var timesArray = [];
 	   			
-	   			while (counterStartDate.unix() <= counterEndDate.unix()) {
-	   				var currentDate = moment.tz(startDateMilli, "UTC").format();
+	   				while (counterStartDate.unix() <= counterEndDate.unix()) {
+	   					console.log(i);
+	   					var currentDate = moment.tz(startDateMilli, "UTC").format();
 
-	   				var splitDate = moment(currentDate).format("M-D-YYYY");
-	   				var dashDate = moment.tz(currentDate, "UTC").format("M/D/YYYY");
-	   				var splitDashDate = moment.tz(currentDate, "UTC").format('YYYY-MM-DD');
-	   				dates.push(counterStartDate.format("M-D-YYYY"));
-	   				var latitude = results[0].geometry.location.lat();
-	   				var longitude = results[0].geometry.location.lng();
-	 					var sunriseurl = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=" + counterStartDate.format("YYYY-MM-DD") + "&callback=mycallback";
-			   		var response = $.ajax({
+	   					var splitDate = moment(currentDate).format("M-D-YYYY");
+	   					var dashDate = moment.tz(currentDate, "UTC").format("M/D/YYYY");
+	   					var splitDashDate = moment.tz(currentDate, "UTC").format('YYYY-MM-DD');
+	   					dates.push(counterStartDate.format("M-D-YYYY"));
+	   					var latitude = results[0].geometry.location.lat();
+	   					var longitude = results[0].geometry.location.lng();
+	 					var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=" + counterStartDate.format("YYYY-MM-DD");
+			   			var response = $.ajax({
 							    		url: sunriseurl,
-							    		dataType: "JSONP",
-							    		success: function(data) {
-								    			function toTimeZone(time) {
-								    				if ($("#zone").val().toString() === "current") {
-								    					var date = new Date(counterStartDate.format("M/D/YYYY") + " " + time + " UTC");
-								    					return moment(date).format("hh:mm:ss a");
-								    				} else {
-								    					var timezone = $("#zone").val();
-									    				var date = new Date(splitDashDate + " " + time + " UTC");
-									    				return moment(date).tz(timezone).format("hh:mm:ss a");
-								    				}
-								    			}
+							    		dataType: "jsonp",
+							    		jsonpCallback: 'window.sunriseCall'
 
-													function toSeconds(time) {
-														time = time.split(":");
-														var seconds = time[2].split(" ")[0];
-														var ampm = time[2].split(" ")[1];
-														var timeInSeconds = ((parseInt(time[0])*60)*60) + (parseInt(time[1])*60) + (parseInt(seconds));
-														if (ampm === "pm") {
-															timeInSeconds += 43200;
-														}
-														var timeFraction = timeInSeconds/86400
-														return timeFraction;
-													}
-
-													var timesObject = {
-								    				'date': '',
-								    				'unixDate': '',
-								    				'sunrise': toTimeZone(data.results.sunrise),
-								    				'sunset': toTimeZone(data.results.sunset),
-								    				'dayLength': data.results.day_length,
-								    				'astroTwilightBegin': toTimeZone(data.results.astronomical_twilight_begin),
-								    				'astroTwilightEnd': toTimeZone(data.results.astronomical_twilight_end),
-								    				'civilTwilightBegin': toTimeZone(data.results.civil_twilight_begin),
-								    				'civilTwilightEnd': toTimeZone(data.results.civil_twilight_end),
-								    				'nauticalTwilightBegin': toTimeZone(data.results.nautical_twilight_begin),
-								    				'nauticalTwilightEnd': toTimeZone(data.results.nautical_twilight_end),
-								    				'solarNoon': toTimeZone(data.results.solar_noon)
-													}
-
-   												timesArray.push(timesObject);
+							    	});
 
 
-								    		$("#sunrises").append("<tr class='day day" + i + "'><td class='date date" + i + "'>" + "</td><td>" + toTimeZone(data.results.sunrise) + "</td><td>" + toTimeZone(data.results.sunset) + "</td><td>" + data.results.day_length + "</td><td><button class='btn btn-default' data-toggle='modal' data-target='#modal" + i + "'>More Info</button></td></tr>");
-							    			$(".modals").append('<div class="modal fade" id="modal' + i + '"tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title date-title' + i + '"></h4></div><div class="modal-body">' + '<div class="modal-details">Astronomical twilight Begins: ' + toTimeZone(data.results.astronomical_twilight_begin) + '</div>' + '<div class="modal-details">Astronomical Twilight Ends: ' + toTimeZone(data.results.astronomical_twilight_end) + '</div><div class="modal-details">Civil Twilight Begins: ' + toTimeZone(data.results.civil_twilight_begin) + '</div><div class="modal-details">Civil Twilight Ends: ' + toTimeZone(data.results.civil_twilight_end) + '</div>' + '<div class="modal-details">Nautical Twilight Begins: ' + toTimeZone(data.results.nautical_twilight_begin) + '</div><div class="modal-details">Nautical Twilight Ends: ' + toTimeZone(data.results.nautical_twilight_end) + '</div><div class="modal-details">Solar Noon: ' + toTimeZone(data.results.solar_noon) + '</div><div class="modal-footer"></div></div></div></div>');
-						    			  i += 1;
-							    			return data;
-							    		}
-										}).done(function() {
-											for (var i = 0; i <= dates.length; i++){
-												$(".date" + i).text(dates[i]);
-												$(".date-title" + i).text(dates[i]);
-											}
+						window.sunriseCall = function(data) {
+					    	console.log(data);
+							function toTimeZone(time) {
+			    				if ($("#zone").val().toString() === "current") {
+			    					var date = new Date(counterStartDate.format("M/D/YYYY") + " " + time + " UTC");
+			    					return moment(date).format("hh:mm:ss a");
+			    				} else {
+			    					var timezone = $("#zone").val();
+				    				var date = new Date(splitDashDate + " " + time + " UTC");
+				    				return moment(date).tz(timezone).format("hh:mm:ss a");
+			    				}
+			    			}
 
-											for (var i = 0; i < timesArray.length; i++) {
-												timesArray[i].unixDate = moment(dates[i]).unix();
-												timesArray[i].date = moment(dates[i]).format("MM-DD-YYYY");
-											}
+							function toSeconds(time) {
+								time = time.split(":");
+								var seconds = time[2].split(" ")[0];
+								var ampm = time[2].split(" ")[1];
+								var timeInSeconds = ((parseInt(time[0])*60)*60) + (parseInt(time[1])*60) + (parseInt(seconds));
+								if (ampm === "pm") {
+									timeInSeconds += 43200;
+								}
+								var timeFraction = timeInSeconds/86400
+								return timeFraction;
+							}
 
-											var endDateUnformat = moment($scope.endDate).utc().startOf('day').format("M-D-YYYY");
-											$("#visualisation").empty();
-											dthreeService.makeGraph(timesArray);
+							var timesObject = {
+			    				'date': '',
+			    				'unixDate': '',
+			    				'sunrise': toTimeZone(data.results.sunrise),
+			    				'sunset': toTimeZone(data.results.sunset),
+			    				'dayLength': data.results.day_length,
+			    				'astroTwilightBegin': toTimeZone(data.results.astronomical_twilight_begin),
+			    				'astroTwilightEnd': toTimeZone(data.results.astronomical_twilight_end),
+			    				'civilTwilightBegin': toTimeZone(data.results.civil_twilight_begin),
+			    				'civilTwilightEnd': toTimeZone(data.results.civil_twilight_end),
+			    				'nauticalTwilightBegin': toTimeZone(data.results.nautical_twilight_begin),
+			    				'nauticalTwilightEnd': toTimeZone(data.results.nautical_twilight_end),
+			    				'solarNoon': toTimeZone(data.results.solar_noon)
+							}
 
-														}); //end of api call
-						startDateMilli += 86400000;
-						counterStartDate = counterStartDate.add(1, 'days');
-			   	} //end of while loop
+   							timesArray.push(timesObject);
+
+
+							$("#sunrises").append("<tr class='day day" + 
+							i + "'><td class='date date" + i + 
+							"'>" + "</td><td>" + 
+							toTimeZone(data.results.sunrise) + 
+							"</td><td>" + toTimeZone(data.results.sunset) + 
+							"</td><td>" + data.results.day_length + 
+							"</td><td><button class='btn btn-default' data-toggle='modal' data-target='#modal" + i + 
+							"'>More Info</button></td></tr>");
+
+						    $(".modals").append('<div class="modal fade" id="modal' + i + 
+						    '"tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title date-title' +
+						     i + '"></h4></div><div class="modal-body">' 
+						     + '<div class="modal-details">Astronomical twilight Begins: ' 
+						     + toTimeZone(data.results.astronomical_twilight_begin) + 
+						     '</div>' + '<div class="modal-details">Astronomical Twilight Ends: ' 
+						     + toTimeZone(data.results.astronomical_twilight_end) + 
+						     '</div><div class="modal-details">Civil Twilight Begins: ' 
+						     + toTimeZone(data.results.civil_twilight_begin) + 
+						     '</div><div class="modal-details">Civil Twilight Ends: ' 
+						     + toTimeZone(data.results.civil_twilight_end) + 
+						     '</div>' + '<div class="modal-details">Nautical Twilight Begins: ' 
+						     + toTimeZone(data.results.nautical_twilight_begin) + 
+						     '</div><div class="modal-details">Nautical Twilight Ends: ' + 
+						     toTimeZone(data.results.nautical_twilight_end) + 
+						     '</div><div class="modal-details">Solar Noon: ' + 
+						     toTimeZone(data.results.solar_noon) + 
+						     '</div><div class="modal-footer"></div></div></div></div>');
+						    			  
+						    i += 1;
+
+							for (var j = 0; j <= dates.length; j++) {
+								$(".date" + j).text(dates[j]);
+								$(".date-title" + j).text(dates[j]);
+							}
+
+							for (var k = 0; k < timesArray.length; k++) {
+								timesArray[k].unixDate = moment(dates[k]).unix();
+								timesArray[k].date = moment(dates[k]).format("MM-DD-YYYY");
+							}
+
+							var endDateUnformat = moment($scope.endDate).utc().startOf('day').format("M-D-YYYY");
+							$("#visualisation").empty();
+							dthreeService.makeGraph(timesArray);
+							return data;
+
+						}//end sunriseCall
+								
+
+								//call this when api call returns
+								
+								startDateMilli += 86400000;
+								counterStartDate = counterStartDate.add(1, 'days');
+								
+			   		} //end of while loop
 
 			} //end of if statement
 
@@ -126,7 +163,7 @@ app.controller('MainCtrl', [
 		$scope.address = '';
 		$scope.city = '';
 		$scope.state = '';
-	}; //end of getTimes();
+	} //end of getTimes();
 
 	function init() {
 		var oneWeekAgo = new Date();
