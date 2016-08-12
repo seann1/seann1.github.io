7c734e7e368d50014d6787650c6041c61fd8a624
Sean Niesen <seanniesen@gmail.com>
2016-08-12 16:17:50 -0700

    fixed sunrise

--- sunrise/app.js
+++ sunrise/app.js
@@ -50,7 +50,7 @@ app.controller('MainCtrl', [
 	   					dates.push(counterStartDate.format("M-D-YYYY"));
 	   					var latitude = results[0].geometry.location.lat();
 	   					var longitude = results[0].geometry.location.lng();
-	 					var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=" + counterStartDate.format("YYYY-MM-DD");
+	 					var sunriseurl = "https://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=" + counterStartDate.format("YYYY-MM-DD");
 			   			var response = $.ajax({
 							    		url: sunriseurl,
 							    		dataType: "jsonp",
