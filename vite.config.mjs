2a6a96c57d849c6b1e478cb52dad712fff323a5e
sean N <sniesen@myhealthily.com>
2024-05-16 22:20:44 -0700

    fix psd remake

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,5 @@
 import glsl from 'vite-plugin-glsl';
+// import { createHtmlPlugin } from 'vite-plugin-html';
 import path from "path";
 import { defineConfig } from 'vite';
 
@@ -7,5 +8,30 @@ export default defineConfig({
 	build: {
 		outDir: path.resolve(__dirname, 'docs')
 	},
-	plugins: [glsl()],
+	rollupOptions: {
+		input: 'src/main.js', // Explicitly define the entry point
+	},
+	plugins: [
+		glsl(),
+		// createHtmlPlugin({
+		// 	pages: [
+		// 		{
+		// 			entry: 'src/main.js',
+		// 			filename: 'src/index.html',
+		// 			// template: 'src/index.html',
+		// 			// injectOptions: {
+		// 			// 	data: {
+		// 			// 		title: 'index',
+		// 			// 		// injectScript: `<script src="./inject.js"></script>`,
+		// 			// 	},
+		// 			// }
+		// 		},
+		// 		{
+		// 			entry: 'src/psd-remake/index.html',
+		// 			filename: 'src/psd-remake.html',
+		// 			template: 'src/psd-remake/index.html',
+		// 		}
+		// 	]
+		// })
+	]
 });
