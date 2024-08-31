4b9159dcc687b19b21eef5188eb6bab604cfcd47
SeanN <sniesen@myhealthily.com>
2024-08-31 12:16:07 -0700

    add marquee

--- src/main.js
+++ src/main.js
@@ -2,12 +2,47 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls';
 const canvasElm = document.getElementById('threeCanMain');
 const renderer = new THREE.WebGLRenderer({ canvas: canvasElm, alpha: true });
+
+/**
+ * Initialize a marquee effect for an HTML element.
+ *
+ * @param {HTMLElement} element - The element to apply the marquee effect to.
+ * @param {number} [repeatCount=7] - The number of times to repeat the element's content.
+ * @param {number} [step=1] - The step size for the marquee animation.
+ */
+const startMarquee = (element, repeatCount = 7, step = 1) => {
+    /**
+     * Function that animates the marquee effect.
+     */
+    const animateMarquee = () => {
+        position = position < width ? position + step : 1;
+        element.style.marginLeft = `${-position}px`;
+        element.style.overflow = 'hidden';
+        element.style.whiteSpace = 'nowrap';
+        requestAnimationFrame(animateMarquee);
+    };
+    
+    let position = 0;
+    const space = '';
+    const content = element.innerHTML;
+    element.innerHTML = Array(repeatCount).fill(content + space).join('');
+    element.style.position = 'absolute';
+    const width = element.clientWidth + 1;
+    element.style.position = '';
+    
+    // Start the marquee animation
+    animateMarquee();
+};
+
 window.onload = function () {
     setTimeout(() => {
         document.getElementById('loading-screen').style.display = 'none';
     }, '50');
     renderer.setSize(window.innerWidth, window.innerHeight);
+    // Example usages:
+    startMarquee(document.getElementById('marquee'), 16, 0.5); // Customize repeatCount and step
 };
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -145,14 +180,6 @@ function animate() {
     group.rotation.y = currentRotationY*0.05;
     
     group.children.forEach((child) => {
-        
-        // Lerp the color
-        // child.material.color.lerp(child.userData.targetColor, 0.01);
-        //
-        // // If the color is close to the target color, set a new target color
-        // if (child.material.color.distanceTo(child.userData.targetColor) < 0.01) {
-        //     child.userData.targetColor.set(Math.random(), Math.random(), Math.random());
-        // }
         
         child.rotation.x += 0.005;
         child.rotation.y += 0.005;
